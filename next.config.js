ce718d7b84344a4c69f25596ef9f2aefcdf6d601
Pearel Nazareth <pearel@sensehq.com>
2024-01-14 22:00:17 +0530

    rewrite next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   output: "export",
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+
   images: {
     domains: ["images.unsplash.com"],
   },
 };
+
+module.exports = nextConfig;
